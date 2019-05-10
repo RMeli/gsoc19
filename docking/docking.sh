197320b191d0b970421fb135115d4e147bf5de63
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-05-10 17:22:49 +0100

    docking pipeline

--- docking/docking.sh
+++ docking/docking.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 
-# smina exec
-smina=~/software/Autodock/Smina/smina.static
-
-# scripts
-scripts=../scripts
+source path.var
 
 # Store working directory
 wd=$PWD
 
 for dataset in "refined"
 do
-    for dir in $(ls -d ${dataset}/????)
+    for dir in $(ls -d ${pdbbind}/${dataset}/10??)
     do
 
- 	# PDB name
-	system=$(basename -a ${dir})
+ 	    # PDB name
+	    system=$(basename -a ${dir})
 
-    # Compute ligand center
-    python ${scripts}/python/ligand_center.py ${dir}/${system}_ligand.pdbqt \
-        -o ${dir}/lig_center.dat
+        # Docking working directory
+        ddir=${dataset}/${system}
+        mkdir -p ${ddir}
+        mkdir -p ${ddir}/logs
 
-    # Get center
-    c_x=$(cat ${dir}/lig_center.dat | awk '{print $1}')
-    c_y=$(cat ${dir}/lig_center.dat | awk '{print $2}')
-    c_z=$(cat ${dir}/lig_center.dat | awk '{print $3}')
+        # Ligand and receptor
+        ligand=${dir}/${system}_ligand.mol2
+        receptor=${dir}/${system}_protein.pdb
 
-    # Docking parameters
-    l_box=23.5
+        # Compute ligand center
+        python3.6 ${pscripts}/ligand_center.py ${ligand} \
+            -o ${ddir}/center.dat
 
-    # Run docking
-    ${smina} -r ${dir}/${system}_protein.pdbqt -l ${dir}/${system}_ligand.pdbqt \
-        --flexdist_ligand ${dir}/${system}_ligand.pdbqt --flexdist 3 \
-        --center_x ${c_x} --center_y ${c_y} --center_z ${c_z} \
-        --size_x ${l_box} --size_y ${l_box} --size_z ${l_box} \
-        --exhaustiveness 8 --num_modes 20 --cpu 6 \
-        --log ${dir}/logs/smina.log
-		
+        # Get center
+        c_x=$(cat ${ddir}/center.dat | awk '{print $1}')
+        c_y=$(cat ${ddir}/center.dat | awk '{print $2}')
+        c_z=$(cat ${ddir}/center.dat | awk '{print $3}')
+
+        # Docking parameters
+        source docking.var
+
+        # Run docking
+        ${smina} -r ${receptor} -l ${ligand} \
+            --flexdist_ligand ${ligand} --flexdist 3 \
+            --center_x ${c_x} --center_y ${c_y} --center_z ${c_z} \
+            --size_x ${l_box} --size_y ${l_box} --size_z ${l_box} \
+            --exhaustiveness ${exhaustiveness} --num_modes ${num_modes} --cpu ${cpu} \
+            --out ${ddir}/smina.pdb --out_flex ${ddir}/flex.pdb \
+            2>&1 | tee ${ddir}/logs/smina.log
+
+        # Split docking results
+        mkdir ${ddir}/dock ${ddir}/flex
+        ${obabel} -m -ipdb ${ddir}/smina.pdb -opdb -O ${ddir}/dock/${system}_dock-.pdb \
+            2>&1 | tee ${ddir}/logs/obabel_dock.log
+        ${obabel} -m -ipdb ${ddir}/flex.pdb -opdb -O ${ddir}/flex/${system}_flex-.pdb \
+            2>&1 | tee ${ddir}/logs/obabel_flex.log
+
     done
 done
