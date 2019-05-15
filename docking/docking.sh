31893f767135298fc6f29b7a7730f17e33a4571c
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-05-15 17:37:31 +0100

    autobox for docking

--- docking/docking.sh
+++ docking/docking.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source path.var
+source variables/paths
 
 # Store working directory
 wd=$PWD
@@ -10,8 +10,8 @@ do
     for dir in $(ls -d ${pdbbind}/${dataset}/[1-3]???)
     do
 
- 	    # PDB name
-	    system=$(basename -a ${dir})
+ 	# PDB name
+	system=$(basename ${dir})
 
         # Docking working directory
         ddir=${dataset}/${system}
@@ -22,33 +22,16 @@ do
         ligand=${dir}/${system}_ligand.mol2
         receptor=${dir}/${system}_protein.pdb
 
-        # Compute ligand center
-        python3.6 ${pscripts}/ligand_center.py ${ligand} \
-            -o ${ddir}/center.dat
-
-        # Get center
-        c_x=$(cat ${ddir}/center.dat | awk '{print $1}')
-        c_y=$(cat ${ddir}/center.dat | awk '{print $2}')
-        c_z=$(cat ${ddir}/center.dat | awk '{print $3}')
-
         # Docking parameters
-        source docking.var
+        source variables/docking
 
         # Run docking
         ${smina} -r ${receptor} -l ${ligand} \
             --flexdist_ligand ${ligand} --flexdist 3 \
-            --center_x ${c_x} --center_y ${c_y} --center_z ${c_z} \
-            --size_x ${l_box} --size_y ${l_box} --size_z ${l_box} \
-            --exhaustiveness ${exhaustiveness} --num_modes ${num_modes} --cpu ${cpu} \
+            --autobox_ligand ${ligand} --autobox_add ${autobox_add} \
+	    --exhaustiveness ${exhaustiveness} --num_modes ${num_modes} --cpu ${cpu} \
             --out ${ddir}/dock.pdb --out_flex ${ddir}/flex.pdb \
             2>&1 | tee ${ddir}/logs/smina.log
-
-        # Split docking results
-        mkdir ${ddir}/dock ${ddir}/flex
-        ${obabel} -m -ipdb ${ddir}/dock.pdb -opdb -O ${ddir}/dock/${system}_dock-.pdb \
-            2>&1 | tee ${ddir}/logs/obabel_dock.log
-        ${obabel} -m -ipdb ${ddir}/flex.pdb -opdb -O ${ddir}/flex/${system}_flex-.pdb \
-            2>&1 | tee ${ddir}/logs/obabel_flex.log
 
     done
 done
