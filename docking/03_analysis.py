8ab2f935c99065697ac1d061a3bb088d01f7e229
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-03 18:24:00 +0100

    rotatable bonds

--- docking/03_analysis.py
+++ docking/03_analysis.py
@@ -1,4 +1,5 @@
 import MDAnalysis as mda
+import pybel
 
 import numpy as np
 import pandas as pd
@@ -40,14 +41,6 @@ for dataset in datasets:
         data = pd.DataFrame([[n_modes, n_flex]], index=[system], columns=col_names)
         df = df.append(data)
 
-# Plot histogram
-bins=np.arange(22) - 0.5 # Center-aligned bins
-plt.hist(df["n_modes"].values, bins=bins, rwidth=0.9)
-plt.xticks(range(21))
-plt.xlim([-1,21])
-plt.xlabel("Number of modes")
-plt.savefig("analysis/plots/n_modes.pdf")
-
 print("\nDescription:")
 print(df.astype(int).describe())
 
@@ -56,3 +49,23 @@ np.savetxt("analysis/noflex.dat", np.array(failed), "%s")
 
 print(f"\nNo conformations found ({len(no_conf)}):\n", *no_conf)
 np.savetxt("analysis/noconf.dat", np.array(no_conf), "%s")
+
+# Number of rotatable bonds for systems where no conformation is found
+n_rot_noconf = []
+for nc in no_conf:
+    path = os.path.join("../PDBbind18", nc)
+    name = os.path.split(nc)[-1]
+    ligand = os.path.join(path, name + "_ligand.mol2")
+
+    for mol in pybel.readfile("mol2", ligand):
+        n_rot_noconf.append(mol.OBMol.NumRotors())
+
+print(f"\nNumber of rotable bond ({len(no_conf)}):\n", *n_rot_noconf)
+
+# Plot histogram for number of modes
+bins=np.arange(22) - 0.5 # Center-aligned bins
+plt.hist(df["n_modes"].values, bins=bins, rwidth=0.9)
+plt.xticks(range(21))
+plt.xlim([-1,21])
+plt.xlabel("Number of modes")
+plt.savefig("analysis/plots/n_modes.pdf")
