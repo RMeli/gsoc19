d2ebb760615ae39391eef1283d1043f481bfc714
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-10 21:06:02 +0100

    docking from list

--- docking/docking.sh
+++ docking/docking.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
+list=analysis/noconf.dat
+
 source variables/paths
 
 # Store working directory
 wd=$PWD
 
-for dataset in "refined"
+for path in $(cat ${list})
 do
-    for dir in $(ls -d ${pdbbind}/${dataset}/[1]???)
-    do
-
  	# PDB name
-	system=$(basename ${dir})
+	system=$(basename ${path})
 
-        # Docking working directory
-        ddir=${dataset}/${system}
-        mkdir -p ${ddir}
-        mkdir -p ${ddir}/logs
+    # PDBbind directory
+    dir=${pdbbind}/${path}
 
-        # Ligand and receptor
-        ligand=${dir}/${system}_ligand.mol2
-        receptor=${dir}/${system}_protein.pdb
+    # Docking working directory
+    ddir=${path}
+    mkdir -p ${ddir}
+    mkdir -p ${ddir}/logs
 
-        # Docking parameters
-        source variables/docking
+    # Ligand and receptor
+    ligand=${dir}/${system}_ligand.mol2
+    receptor=${dir}/${system}_protein.pdb
 
-        # Run docking
-        ${smina} -r ${receptor} -l ${ligand} \
-            --flexdist_ligand ${ligand} --flexdist 3 \
-            --autobox_ligand ${ligand} --autobox_add ${autobox_add} \
-	        --exhaustiveness ${exhaustiveness} --num_modes ${num_modes} --cpu ${cpu} \
-            --out ${ddir}/dock.pdb --out_flex ${ddir}/flex.pdb \
-            2>&1 | tee ${ddir}/logs/smina.log
+    # Docking parameters
+    source variables/docking
 
-    done
+    # Run docking
+    echo "--receptor ${receptor}" | tee ${ddir}/logs/smina.log
+    echo "--ligand ${ligand}" | tee ${ddir}/logs/smina.log
+    echo "--autobox_add ${autobox_add}" | tee ${ddir}/logs/smina.log
+    echo "--exaustiveness ${exhaustiveness}" | tee ${ddir}/logs/smina.log
+    echo "--num_modes ${num_modes}" | tee ${ddir}/logs/smina.log
+    echo "--cpu ${cpu}" | tee ${ddir}/logs/smina.log
+    echo "--out ${ddir}/dock.pdb" | tee ${ddir}/logs/smina.log
+    echo "--out_flex ${ddir}/flex.pdb" | tee ${ddir}/logs/smina.log
+
+    ${smina} -r ${receptor} -l ${ligand} \
+        --flexdist_ligand ${ligand} --flexdist 3 \
+        --autobox_ligand ${ligand} --autobox_add ${autobox_add} \
+	    --exhaustiveness ${exhaustiveness} --num_modes ${num_modes} --cpu ${cpu} \
+        --out ${ddir}/dock.pdb --out_flex ${ddir}/flex.pdb \
+        2>&1 | tee ${ddir}/logs/smina.log
 done
