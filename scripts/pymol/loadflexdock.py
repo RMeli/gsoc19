fa2e99268d02653b0052ca97a6fc08205da3ffb8
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-13 10:27:11 +0100

    added crystal ligand to pymol script

--- scripts/pymol/loadflexdock.py
+++ scripts/pymol/loadflexdock.py
@@ -4,23 +4,31 @@ import os
 
 num_modes = 20 # Number of docking modes
 
-docksel = {f"{i}" : f"ligand_{i:04d}" for i in range(1,num_modes+1)}
-flexsel = {f"{i}" : f"flexres_{i:04d}" for i in range(1,num_modes+1)}
-
 def loadflexdock(system, dataset, idx=1, flexdist=3, pdbbindpath="../PDBbind18", dockingpath=""):
+
+    # Convert string idx to number
+    idx = int(idx)
 
     print(f"Loading {dataset}/{system}")
 
-    ligandpath = os.path.join(dockingpath, dataset, system, f"dock.pdb")
-    flexrespath = os.path.join(dockingpath, dataset, system, f"flex.pdb")
-    receptorpath = os.path.join(pdbbindpath, dataset, system, f"{system}_protein.pdb")
+    ligandpath = os.path.join(dockingpath, dataset, system, f"dock.pdb") # Docked ligands
+    flexrespath = os.path.join(dockingpath, dataset, system, f"flex.pdb") # Flexible residues
+    cligandpath = os.path.join(pdbbindpath, dataset, system, f"{system}_ligand.mol2") # Crystal ligand
+    receptorpath = os.path.join(pdbbindpath, dataset, system, f"{system}_protein.pdb") # Crystal receptor
 
     # Load ligand and receptor
     # When loading a multi-MODEL PDB file, PyMol appends 000X to the selection name
     # DOCKSEL and FLEXSEL dictionaries maps an index to the actual selection
     cmd.load(ligandpath, "ligand") # Selection name: ligand_0001, ligand_0002, ...
+    cmd.load(cligandpath, "cligand") # Selection name: ligand_0001, ligand_0002, ...
     cmd.load(flexrespath, "flexres") # Selection name: flexres_0001, flexres_0002, ...
     cmd.load(receptorpath, "receptor") # Selection name: receptor
+
+    # Map selection name to rank (0 is the crystal)
+    docksel = {i : f"ligand_{i:04d}" for i in range(1,num_modes+1)}
+    docksel[0] = "cligand" # Add index 0 for crystal
+    flexsel = {i : f"flexres_{i:04d}" for i in range(1,num_modes+1)}
+    flexsel[0] = f"receptor within {flexdist} of {docksel[idx]}" # Add index 0 for crystal
     
     # Hide everything and show only receptor and single ligand
     cmd.hide("all")
@@ -41,7 +49,7 @@ def loadflexdock(system, dataset, idx=1, flexdist=3, pdbbindpath="../PDBbind18",
     # Get residue index of atoms within FLEXDIST from the ligand
     stored.list = []
     cmd.iterate(
-        f"all within {flexdist} of {docksel[idx]}", # Selection
+        f"receptor within {flexdist} of {docksel[0]}", # Selection
         "stored.list.append((resn, resi, chain))" # Action
     )
 
