bbd37a377d27267e829347217a741ea4a07f7fe7
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-24 15:15:54 +0100

    flexible residues rmsd test (slow)

--- datasets/flexdock/02_score.sh
+++ datasets/flexdock/02_score.sh
@@ -2,7 +2,7 @@
 
 source variables/paths
 
-csv_header="name,system,rank,rmsd_lig,score"
+csv_header="system,rank,rmsd_lig,rmsd_flex,score"
 
 allscores="analysis/allscores.csv"
 rm -f ${allscores}
@@ -24,15 +24,26 @@ do
         echo ${csv_header} > ${csvfile}
 
         ligand_crystal=${pdbbind}/${dataset}/${system}/${system}_ligand.mol2
+        protein_crystal=${pdbbind}/${dataset}/${system}/${system}_protein.pdb
 
         for ligand in $(ls ${dir}/${system}_ligand-*.pdb)
         do
+            # Ligand name and rank
             name=$(basename $ligand .pdb)
             rank=$( echo $name | sed "s#${system}_ligand-##g" )
-            rmsd=$(${obrms} ${ligand_crystal} ${ligand} | awk  '{print $2}')
+
+            # Ligand RMSD (with OpenBabel)
+            rmsd_lig=$(${obrms} ${ligand_crystal} ${ligand} | awk  '{print $2}')
+
+            # Flexible residues RMSD (with MDAnalysis)
+            flex=${dir}/${system}_flex-${rank}.pdb
+            protein=${dir}/${system}_protein-${rank}.pdb
+            rmsd_flex=$(python3.6 ${pscripts}/flexrmsd.py ${flex} ${protein} ${protein_crystal})
+
+            # Score (from ligand file)
             score=$(grep "minimizedAffinity" ${ligand} | awk '{print $3}')
 
-            info="${name},${system},${rank},${rmsd},${score}"
+            info="${name},${system},${rank},${rmsd_lig},${rmsd_flex},${score}"
             echo ${info} >> ${csvfile}
             echo ${info} >> ${allscores}
         done
