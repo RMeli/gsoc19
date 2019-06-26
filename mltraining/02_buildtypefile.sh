234c66ac2fe60ffd292d0df93d29f430c17c8f40
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-26 18:29:08 +0100

    types files
    many systems are not found in the probis fold

--- mltraining/02_buildtypefile.sh
+++ mltraining/02_buildtypefile.sh
@@ -8,7 +8,7 @@ foldcsv=probis.csv
 # Remove previous files (python script appends)
 rm ${typedir}/*.types
 
-python ${pscripts}/buildtypefiles.py \
+python3.6 ${pscripts}/buildtypefiles.py \
     ${database} \
     ${typedir} \
     --folds ${foldir}/${foldcsv}
