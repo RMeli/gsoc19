b0a2d502658f0ea944f8c0d6bc674c6e5c207b05
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-28 10:46:18 +0100

    generate smiles for pdbbind ligands

---
+++ PDBbind18/02_list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List all paths to PDBbind18 systems
+
+lname=pdbbind18
+
+rm -f ${lname}.list
+
+for dataset in "refined" "other"
+do
+    for dir in $(ls -d ${dataset}/????) 
+    do
+	system=$(basename ${dir})
+
+	echo "${dataset}/${system}" >> ${lname}.list 	
+    done
+done
---
+++ PDBbind18/03_smiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Compute SMILES strings for all the ligands
+
+for dataset in "test"
+do
+    for dir in $(ls -d ${dataset}/????) 
+    do
+        system=$(basename ${dir})
+        
+        # Input and output names
+        ligname=${dir}/${system}_ligand.mol2
+        sminame=${dir}/${system}_ligand.smi
+
+        # Convert mol2 to smi and extract SMILES string only
+        smiles=$(obabel -imol2 -osmi ${ligname} | awk '{print $1}')
+
+        # Save SMILES to file
+        echo ${smiles} > ${sminame}
+    done
+done
