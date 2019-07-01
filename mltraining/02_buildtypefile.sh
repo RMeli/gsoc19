16137483556a99031f3de11071cee9062e0bcb72
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-01 11:53:00 +0100

    concatenate all types files

--- mltraining/02_buildtypefile.sh
+++ mltraining/02_buildtypefile.sh
@@ -6,3 +6,6 @@ source variables/annotation
 python3.6 ${pscripts}/buildtypefiles.py \
     ${database} \
     ${typedir}
+
+# Concatenate all .types files into one
+cat ${typedir}/????/????.types > ${typedir}/all.types
