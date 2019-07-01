2960356f1852f48d184cdb936f6baded5f56a764
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-01 14:14:55 +0100

    molcache script

--- mltraining/04_molcache.sh
+++ mltraining/04_molcache.sh
@@ -6,6 +6,9 @@ python ${gscripts}/create_caches2.py \
     "${typedir}/alltrain0.types" \
     "${typedir}/alltrain1.types" \
     "${typedir}/alltrain2.types" \
+    "${typedir}/alltest0.types" \
+    "${typedir}/alltest1.types" \
+    "${typedir}/alltest2.types" \
     -c 1 -d ${typedir} \
     --recmolcache "${typedir}/rec.molcache2" \
     --ligmolcache "${typedir}/lig.molcache2"
