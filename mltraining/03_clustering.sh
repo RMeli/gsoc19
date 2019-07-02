ebddbf78051aa368c0a2ec53625d4c6f578d4e27
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-02 11:33:20 +0100

    parallel clustering pipeline

--- mltraining/03_clustering.sh
+++ mltraining/03_clustering.sh
@@ -2,32 +2,42 @@
 
 source variables/paths
 
+n_cpus=2
+
 # Extract ligands SMILES and receptors sequences
 # Format: PDBCODE | SMILES | SEQ
 python ${gscripts}/compute_seqs.py \
     --pdbfiles ../PDBbind18/pdbfiles.lst \
     --out ${clusterdir}/seqs.dat
 
-# Compute similarity of one system with all other systems
-# TODO: Parallelize this loop
-rows=$(wc ${typedir}/seqs.dat | awk '{print $1}')
-for row in $(seq 0 $((${rows}-1)))
-do
+# Export variables for GNU parallel
+export rows=$(wc ${clusterdir}/seqs.dat | awk '{print $1}')
+export gscripts=${gscripts}
+export clusterdir=${clusterdir}
+
+# Function to compute a single row of the similarity matrix
+compute_row(){
+    row=$1
+
     echo ">>> Computing row $((${row}+1)) of ${rows}..."
 
     python ${gscripts}/compute_row.py \
-        --pdbseqs ${typedir}/seqs.dat \
+        --pdbseqs ${clusterdir}/seqs.dat \
         --row ${row} \
         --out ${clusterdir}/row-${row}
+}
+
+# Export function for GNU parallel
+export -f compute_row
+
+# Compute similarity of one system with all other systems
+for row in $(seq 0 ${n_cpus} $((${rows}-1)))
+do
+    s=$(seq ${row} $((${row} + ${n_cpus} - 1)))
+    echo $s
+    parallel compute_row ::: ${s}
 done
 
 # Combine similarity measures
 python ${gscripts}/combine_rows.py ${clusterdir}/row-*
 mv matrix.pickle ${clusterdir}/matrix.pickle
-
-# Create folds
-python ${gscripts}/clustering.py \
-    --cpickle  ${clusterdir}/matrix.pickle \
-    --input ${typedir}/all.types \
-    --output ${typedir}/all
-
