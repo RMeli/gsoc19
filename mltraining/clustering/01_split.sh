d9a744c5be5fc81e40ec2305385434ddf2cbe359
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-04 15:24:01 +0100

    map and split script for clustering on HPC

---
+++ mltraining/clustering/01_split.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+n_nodes=12
+n_cores=24
+
+n=$(( ${n_nodes} * ${n_cores} ))
+
+rows=$(cat seqs.dat | wc -l)
+
+echo "Number of rows: $rows"
+
+rm -f mapping.dat
+
+row=0
+while [ $row -lt $rows ]
+do
+    for i in $(seq 0 $(($n - 1)))
+    do
+	echo $i $row >> mapping.dat
+
+	row=$(( $row + 1 ))
+
+	if [ $row -ge $rows ]
+ 	then
+	    break
+	fi
+    done
+done
+
+split -d -l ${n} mapping.dat splits/split
+
+rm mapping.dat
