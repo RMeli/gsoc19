aad47ff3070ade9708ad876593250f03face51e7
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-05 10:25:17 +0100

    docking cleanup no flexible residues

--- docking/04_cleanup.sh
+++ docking/04_cleanup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 failed=analysis/failed.dat
+noflex=analysis/noflex-refined.dat
 
+# Remove systems failed
 for dir in $(cat ${failed})
+do
+    rm -r ${dir}
+done
+
+# Remove systems without flexible residues
+for dir in $(cat ${noflex})
 do
     rm -r ${dir}
 done
