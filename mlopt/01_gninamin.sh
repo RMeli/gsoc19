c0d27deb8fae0d1ae90188e85508c908f0d3ed77
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-07-27 17:04:53 +0100

    gnina minimisation script updated

--- mlopt/01_gninamin.sh
+++ mlopt/01_gninamin.sh
@@ -5,21 +5,23 @@ list=lists/test.lst
 
 source variables/paths
 
-mkdir -p optimal
+mkdir -p test
 
 for dir in $(cat ${list})
 do
     system=$(basename ${dir})
 
-    outdir=optimal/${system}
+    outdir=test/${system}
     mkdir -p ${outdir}
 
     ligand=${dataroot}/${dir}/${system}_ligand.mol2
     receptor=${dataroot}/${dir}/${system}_protein.pdb
 
     ${gnina} -l ${ligand} -r ${receptor} \
+    --flexdist 3 --flexdist_ligand ${ligand} \
     --cnn default2017 \
     --cnn_scoring --minimize  \
     --gpu \
-    --out ${outdir}/${system}_ligand-min.pdb
+    --out ${outdir}/${system}_ligand-min.pdb\
+    --out_flex ${outdir}/${system}_flex-min.pdb
 done
