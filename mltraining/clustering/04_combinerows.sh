e8a64526e3096e8b24bff999d0c9a356ceb94be0
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-15 13:34:31 +0100

    final combine rows with output

--- mltraining/clustering/04_combinerows.sh
+++ mltraining/clustering/04_combinerows.sh
@@ -4,4 +4,4 @@
 source ../variables/paths
 
 # Combine similarity measures
-python ${gscripts}/combine_rows_lowmem.py rows/row-*
+python ${gscripts}/combine_rows_lowmem.py rows/row-* --out dist-lsim.pickle
