13aaf549a6f1b2e68078d3094c486d2231e990a2
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-17 14:56:16 +0100

    buildtypefile pipeline updated with output train folder

--- mltraining/02_buildtypefile.sh
+++ mltraining/02_buildtypefile.sh
@@ -5,11 +5,19 @@ box_size=28.5
 source variables/paths
 source variables/annotation
 
-python ${pscripts}/buildtypefiles.py \
+datasets="redined other"
+
+traindir=$1
+
+if [[ $traindir == "" ]]
+then
+  echo "TRAINDIR must be specified."
+  exit
+fi
+
+python ${pscripts}/buildtypefile.py \
     ${database} \
     ${typedir} \
     --lmin ${min} --lmax ${max} --fmin ${fmin} --lmax ${fmax} \
-    -L ${box_size}
-
-# Concatenate all .types files into one
-find gninatypes -name "????.types" | xargs cat > ${typedir}/all.types
+    -L ${box_size} -d ${datasets} \
+    -o ${traindir}
