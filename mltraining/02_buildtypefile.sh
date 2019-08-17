8cfa70b97a520820631de07f563f424a8933c5bf
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-17 14:52:41 +0100

    better buildtypefile scripts with single output

--- mltraining/02_buildtypefile.sh
+++ mltraining/02_buildtypefile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+box_size=28.5
+
 source variables/paths
 source variables/annotation
 
 python ${pscripts}/buildtypefiles.py \
     ${database} \
-    ${typedir}
+    ${typedir} \
+    --lmin ${min} --lmax ${max} --fmin ${fmin} --lmax ${fmax} \
+    -L ${box_size}
 
 # Concatenate all .types files into one
 find gninatypes -name "????.types" | xargs cat > ${typedir}/all.types
--- scripts/python/buildtypefiles.py
+++ scripts/python/buildtypefiles.py
@@ -8,20 +8,23 @@ import os
 import pandas as pd
 import re
 
-from typing import Optional
+from inbox import inbox
 
-datasets = ["refined", "other"]
+from typing import Optional
 
 def parse(args: Optional[str] = None) -> ap.Namespace:
 
     parser = ap.ArgumentParser()
 
     parser.add_argument("datapath", type=str, help="Path to database root.")
-    parser.add_argument("outpath", type=str, help="Path to gninatypes root.")
+    parser.add_argument("typespath", type=str, help="Path to gninatypes root.")
     parser.add_argument("--lmin", default=2, type=float)
     parser.add_argument("--lmax", default=4, type=float)
     parser.add_argument("--fmin", default=1, type=float)
     parser.add_argument("--fmax", default=1.5, type=float)
+    parser.add_argument("-L", "--box_size", default=23.5, type=float)
+    parser.add_argument("-o", "--out", default="all.types", type=str)
+    parser.add_argument("-d", "--datasets", nargs="+", default=["refined", "other"], type=str)
 
     args = parser.parse_args()
 
@@ -35,6 +38,8 @@ def write_record(
     lmax: float,
     fmin: float,
     fmax: float,
+    box_size: float,
+    path: float,
     outfile: str,
 ):
 
@@ -45,16 +50,32 @@ def write_record(
 
         ligname = f"{system}_ligand-{rank}"
         recname = ligname.replace("ligand", "protein")
+        flexname = ligname.replace("ligand", "flex")
 
         ligpath = f"{system}/{ligname}.gninatypes"
         recpath = f"{system}/{recname}.gninatypes"
 
+        ligpdbpath = os.path.join(path, f"{system}_ligand-{rank}.pdb")
+        flexpdbpath = os.path.join(path, f"{system}_flex-{rank}.pdb")
+
         rmsd_lig = float(row["rmsd_lig"])
         rmsd_flex = float(row["rmsd_fmax"])
         if rmsd_lig <= lmin and rmsd_flex <= fmin:
-            annotation = 0
+            annotation = 1 # Positive label
+
+            # Check that ligand and flexible residues are within the box
+            ligin, flexin = inbox(ligpdbpath, flexpdbpath, box_size)
+            if ligin == False or flexin == False:
+                continue
+
         elif rmsd_lig >= lmax and rmsd_flex >= fmax:
-            annotation = 1
+            annotation = 0 # Negative label
+
+            # Check that ligand and flexible residues are within the box
+            ligin, flexin = inbox(ligpdbpath, flexpdbpath, box_size)
+            if ligin == False or flexin == False:
+                continue
+
         else:  # Discard (lmin, lmax) and (fmin, fmax) intervals
             continue  # Skip
 
@@ -72,19 +93,17 @@ if __name__ == "__main__":
     # List all folders containing gninatypes files
     dirs = [
         d
-        for d in os.listdir(args.outpath)
-        if re.match("^....$", d) and os.path.isdir(os.path.join(args.outpath, d))
+        for d in os.listdir(args.typespath)
+        if re.match("^....$", d) and os.path.isdir(os.path.join(args.typespath, d))
     ]
 
-    # Loop over folds
-    for system in dirs:
+    with open(args.out, "w") as out:
 
-        typesfile = f"{args.outpath}/{system}/{system}.types"
-
-        with open(typesfile, "w") as out:
+        # Loop over folds
+        for system in dirs:
 
             # Automatically check if system is in refined or other set
-            for dataset in datasets:
+            for dataset in args.datasets:
                 scorepath = os.path.join(
                     args.datapath, dataset, system, f"{system}_score.csv"
                 )
@@ -95,6 +114,9 @@ if __name__ == "__main__":
             # Get system scores
             df_score = pd.read_csv(scorepath)
 
+            # Get  path to database for ligand and flex files
+            path = os.path.join(args.datapath, dataset, system)
+
             write_record(
-                system, df_score, args.lmin, args.lmax, args.fmin, args.fmax, out
+                system, df_score, args.lmin, args.lmax, args.fmin, args.fmax, args.box_size, path, out
             )  # Write record
