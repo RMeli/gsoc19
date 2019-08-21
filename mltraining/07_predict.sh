e31a88931c134ca86f231f5e6e30abe4a5818863
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-21 16:04:04 +0100

    added predictions on the full dataset

--- mltraining/07_predict.sh
+++ mltraining/07_predict.sh
@@ -19,6 +19,8 @@ cd ${traindir}
 # Caffe
 export PYTHONPATH=${gcaffe}:${PYTHONPATH}
 
+# Test on systems contained in original alltestX.types
+# Test on systems 
 # Python 3.6 for Singularity container U18-C101
 for fold in 0 1 2
 do
@@ -30,4 +32,20 @@ do
         -g ${gpu} \
         -s 0 \
         -o test${fold}.out
+done
+
+# Test on gull list of systems (in a given fold) including poses discarded for training
+# Poses with ligand/flex RMSD in [min,max]/[fmin,fmax] are included here
+# See 06_fulltypefiles.sh for more details
+# Python 3.6 for Singularity container U18-C101
+for fold in 0 1 2
+do
+    python3.6 ${gscripts}/predict.py \
+        -m ../${modeldir}/${model} \
+        -d ../${dataroot} \
+        -w *.${fold}_iter_*.caffemodel \
+        -i fulltest${fold}.types \
+        -g ${gpu} \
+        -s 0 \
+        -o fulltest${fold}.out
 done
