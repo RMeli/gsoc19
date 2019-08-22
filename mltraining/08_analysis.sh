313e7ed6b3b440def193b12c12c830424885e3bd
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-22 10:43:50 +0100

    analysis script for mltraining

--- mltraining/08_analysis.sh
+++ mltraining/08_analysis.sh
@@ -19,8 +19,21 @@ python -m scripts.plot.roc \
      -l "Train 0" "Train 1" "Train 2" "Test 0" "Test 1" "Test 2" \
      -o auc.pdf
 
+
+# scripts.plot.pr is available at https://github.com/RMeli/scripts/
 python -m scripts.plot.pr \
     *.?.auc.finaltrain *.?.auc.finaltest \
      -g 0 0 0 1 1 1 \
      -l "Train 0" "Train 1" "Train 2" "Test 0" "Test 1" "Test 2" \
      -o pr.pdf
+
+
+for fold in 0 1 2
+do 
+    echo "cnn_score,smina_score,lig_rmsd,fmax_rmsd,system,smina_rank" > fulltest${fold}.csv
+    head -n -1 fulltest${fold}.out  | awk '{print $1","$9","$7","$8","$4}' | 
+        sed "s#/...._protein-#,#g" | sed 's#\.gninatypes##g' >> fulltest${fold}.csv
+
+    python ../dist.py fulltest${fold}.csv -o test${fold}
+done
+
