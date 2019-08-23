1ee6a4988b1e230cf266868145a3ff3be6788825
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-23 17:45:57 +0100

    mlopt for folds 1 and 2

--- mlopt/02_list.sh
+++ mlopt/02_list.sh
@@ -17,8 +17,8 @@ fi
 
 cd ${optdir}
 
-for fold in 0
+for fold in 0 1 2
 do
   cat ../${traindir}/alltest${fold}.types | awk '{print $2}' | sed "s#..../##g" | \
-    sed "s#_protein-# #g" | sed "s#.gninatypes##g" > test${fold}-systems.lst
+    sed "s#_protein-# #g" | sed "s#.gninatypes##g" > test${fold}.lst
 done
