831d44058d7985f8ebc3d6ea5bbe2e9c5fe452cf
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-08-24 13:37:58 +0100

    analysis

--- datasets/flexdock/07_analysis.sh
+++ datasets/flexdock/07_analysis.sh
@@ -4,9 +4,9 @@ source variables/paths
 
 python ${pscripts}/distrmsd.py \
     analysis/allscores.csv \
-    -mr 5 -b 100 \
+    -mr 3 -b 0.1 \
     -opath analysis/plots
 
-python ${pscripts}/rmsdthreshold.py \
-    analysis/allscores.csv \
-    -opath analysis/plots
+#python ${pscripts}/rmsdthreshold.py \
+#    analysis/allscores.csv \
+#    -opath analysis/plots
