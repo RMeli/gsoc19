990d90f785b75134b726227953b4ca920a8b6874
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-09-09 18:48:44 +0100

    updated docking split script

--- docking/01_split.sh
+++ docking/01_split.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+source variables/paths
+
 # Split PDBbind
 tdir=splits
 mkdir ${tdir}
-split -l 100 -d -a 3 ${pdbbind}/pdbbind18.list ${tdir}/split
+split -l 100 -d -a 3 ${pdbbind}/lists/pdbbind18.lst ${tdir}/split
