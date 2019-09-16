009b4c8ece62a5647089bee41931b5696768cfab
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-09-16 10:38:59 +0100

    array jobs for docking

--- docking/02_docking-sub.sh
+++ docking/02_docking-sub.sh
@@ -10,21 +10,10 @@ wd=$PWD
 jdir=jobs
 mkdir -p ${jdir}
 
-# Splits directory
-tdir=splits
+jobname=${jdir}/docking.job
 
-# Loop over lists of systems (splits, docked by a single job)
-for s in $(ls ${tdir})
-do
-    sname=$(basename ${s})
-    jobname=${jdir}/${sname}.job
+cp templates/${sn}.job ${jobname}
 
-    cp templates/${sn}.job ${jobname}
-
-    sed -i "s#<SPLITNAME>#${s}#g" ${jobname}
-    sed -i "s#<SPLITFILE>#${PWD}/${tdir}/${s}#g" ${jobname}
-
-    ${sub} ${jobname}
+${sub} ${jobname}
  
-    sleep 1 
-done
+sleep 1
