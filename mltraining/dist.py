c49e6a41f4a13a5b68bb99a818629547ee043ea3
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-10-28 14:46:28 +0000

    added distplot option to rmsd distribution script

--- mltraining/dist.py
+++ mltraining/dist.py
@@ -9,6 +9,7 @@ import argparse as ap
 parser = ap.ArgumentParser(description="Compute center of geometry of a molecule.")
 parser.add_argument("fnames", type=str, nargs="+", help="Full test results")
 parser.add_argument("-o", "--output", type=str, default=None)
+parser.add_argument("-g", "--graph", type=str, default=None)
 args = parser.parse_args()
 
 df_RMSD = pd.DataFrame(columns=["name", "RMSD", "Fold", "type", "method"])
@@ -27,7 +28,7 @@ for fold, fname in enumerate(args.fnames):
     df_merged = df_merged.merge(lig_rmsd, on="system")
     df_merged = df_merged.merge(fmax_rmsd, on="system")
 
-    print(df_merged.head())
+    #print(df_merged.head())
 
     df_rmsd = df_merged.drop(columns=["cnn_score", "smina_score","system"]).melt(var_name="name", value_name="RMSD")
     df_rmsd["Fold"] = fold
@@ -51,14 +52,27 @@ for fold, fname in enumerate(args.fnames):
     df_rmsd["type"] = df_rmsd.apply(t, axis=1)
     df_rmsd["method"] = df_rmsd.apply(m, axis=1)
 
-    print(df_rmsd.head())
+    #print(df_rmsd.head())
 
     df_RMSD = pd.concat([df_RMSD, df_rmsd])
 
-g = sns.catplot(x="Fold", y="RMSD", hue="method", col="type", data=df_RMSD, hue_order=["smina", "cnn", "best"], kind="boxen", legend=False, aspect=0.5)
-g.set_titles("{col_name}")
-plt.legend()
-plt.suptitle("Top Pose RMSD Distributions")
-plt.tight_layout()
-plt.savefig(args.output)
-#plt.show()
+print(df_RMSD)
+
+if args.graph == "boxen":
+    plt.figure()
+    g = sns.catplot(x="Fold", y="RMSD", hue="method", col="type", data=df_RMSD, hue_order=["smina", "cnn", "best"], kind="boxen", legend=False)
+    g.set_titles("{col_name}")
+    plt.legend()
+    plt.suptitle("Top Pose RMSD Distributions")
+    plt.tight_layout()
+    plt.savefig(args.output)
+elif args.graph == "distplot":
+    plt.figure()
+    bins = np.linspace(0, max(df_RMSD["RMSD"]), 150)
+    g = sns.FacetGrid(df_RMSD.drop("name", axis=1), col="Fold", row="type", hue="method")
+    g.map(sns.distplot, "RMSD", bins=bins)
+    plt.legend()
+    plt.xlim([0,6])
+    plt.savefig(args.output)
+else:
+    raise Exception(f"{args.graph} is not a valid option.")
