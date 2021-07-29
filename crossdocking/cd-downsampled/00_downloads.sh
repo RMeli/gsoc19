3ea02b4f813b60846ee005038dc001dd2495a96a
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-07-29 14:53:48 +0100

    improve script description

--- crossdocking/cd-downsampled/00_downloads.sh
+++ crossdocking/cd-downsampled/00_downloads.sh
@@ -1,6 +1,8 @@
 #!\bin\bash
 
-# Download data from David Koes website
+# --------------------------------------------------
+# Download data from David Koes website.
+# --------------------------------------------------
 
 wget http://bits.csb.pitt.edu/files/gnina1.0_paper/crossdocked_ds_data.tar.gz
 tar -xvf crossdocked_ds_data.tar.gz
--- crossdocking/cd-downsampled/01_make_gnina_commands.sh
+++ crossdocking/cd-downsampled/01_make_gnina_commands.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# --------------------------------------------------
+# Create reference GNINA commands for docking.
+# --------------------------------------------------
+
 python make_gnina_cmds.py ds_cd_input_pairs.txt --flexdist 3.5 --cnn none
