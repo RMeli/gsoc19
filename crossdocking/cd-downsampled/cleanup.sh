f7e38b613ad8439e889dc4652633a1f0dc9d3f22
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-04 15:26:21 +0100

    cleanup temporary directories

--- crossdocking/cd-downsampled/cleanup.sh
+++ crossdocking/cd-downsampled/cleanup.sh
@@ -5,8 +5,13 @@
 # --------------------------------------------------
 
 for pocket in $(ls carlos_cd)
+#for pocket in AOFB
 do
+    # Cleanup 03_crustal.slurm
     #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
     #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
-    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*.rmsds
+    
+    # Cleanup 05_spyrmsd.slurm
+    rm carlos_cd/${pocket}/PDB_Structures/*.rmsds
+    find carlos_cd/${pocket}/PDB_Structures/ -type d -name '*.d' -exec rm -r {} +
 done
