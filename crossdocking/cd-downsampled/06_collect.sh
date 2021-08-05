88500ffaf6fa3b22fd8ae2c1c7f2352ba52348e5
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-05 13:39:55 +0100

    collect all rmsd calculations

---
+++ crossdocking/cd-downsampled/06_collect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+allscores="carlos_cd/rmsds.csv"
+
+# CSV Header
+csv_header="pocket,protein,ligand,rank,rmsd,obrmsd,flexrmsd,flexobrmsd,fmaxrmsd,score"
+
+# Create a file containing all the scores
+rm -f ${allscores}
+rm -f allscores.tmp # Remove tmp file
+touch allscores.tmp # Create tmp file
+for pocket in $(ls carlos_cd)
+do
+    # Concatenate all score files
+    cat allscores.tmp carlos_cd/${pocket}/PDB_Structures/*.rmsds > tmp
+    mv tmp allscores.tmp
+done
+grep -v ${csv_header} allscores.tmp > ${allscores} # Remove all headers
+echo ${csv_header} | cat - ${allscores} > allscores.tmp # Add top header
+mv allscores.tmp ${allscores}
