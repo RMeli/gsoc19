9a14986ba6209a04a223ac4763c82d8d7ab0d66d
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-05 13:39:42 +0100

    celeanup

--- crossdocking/cd-downsampled/compress.sh
+++ crossdocking/cd-downsampled/compress.sh
@@ -8,9 +8,9 @@
 # re-starting the job).
 # --------------------------------------------------
 
-#for pocket in $(ls carlos_cd)
-for pocket in AOFB
+for pocket in $(ls carlos_cd)
+#for pocket in AOFB
 do
-    gzip carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*.pdb
-    gzip carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*.sdf
+    gzip -f carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*.pdb
+    gzip -f carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*.sdf
 done
