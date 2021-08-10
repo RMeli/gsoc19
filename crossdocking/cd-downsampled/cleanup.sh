f0e2333042df4abbb4219e374248ad22c7c6fb22
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-10 13:39:32 +0100

    remove slurm files in cleanup

--- crossdocking/cd-downsampled/cleanup.sh
+++ crossdocking/cd-downsampled/cleanup.sh
@@ -7,11 +7,18 @@
 for pocket in $(ls carlos_cd)
 #for pocket in AOFB
 do
-    # Cleanup 03_crustal.slurm
-    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
-    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
+    # Cleanup 03_crystal.slurm
+    # Cleanup 04_makeflex.slurm
+    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
+    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
+    rm slurm/crystal/*
+    rm slurm/makeflex/*
     
     # Cleanup 05_spyrmsd.slurm
     rm carlos_cd/${pocket}/PDB_Structures/*.rmsds
     find carlos_cd/${pocket}/PDB_Structures/ -type d -name '*.d' -exec rm -r {} +
+    rm slurm/spyrmsd/*
 done
+
+# Cleanup 06_collect.sh
+rm carlos_cd/rmsds.csv
