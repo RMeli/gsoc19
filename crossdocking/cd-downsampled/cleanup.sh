bdb931600b586bbc8a3b42e69656bc507458607b
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-09-08 09:58:26 +0100

    cleanup more stuff in cleanup script

--- crossdocking/cd-downsampled/cleanup.sh
+++ crossdocking/cd-downsampled/cleanup.sh
@@ -9,16 +9,26 @@ for pocket in $(ls carlos_cd)
 do
     # Cleanup 03_crystal.slurm
     # Cleanup 04_makeflex.slurm
-    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
-    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
-    rm slurm/crystal/*
-    rm slurm/makeflex/*
+    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
+    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
+    #rm slurm/crystal/*
+    #rm slurm/makeflex/*
     
     # Cleanup 05_spyrmsd.slurm
     rm carlos_cd/${pocket}/PDB_Structures/*.rmsds
     find carlos_cd/${pocket}/PDB_Structures/ -type d -name '*.d' -exec rm -r {} +
-    rm slurm/spyrmsd/*
+
+    # Cleanup 09_gningtyper.sh
+    #  .gninatypes file are already cleaned up by commands above (without extension)
 done
 
 # Cleanup 06_collect.sh
 rm carlos_cd/rmsds.csv
+
+# Cleanup 10_annotation.py
+rm files/test_*
+rm files/train_*
+
+# Cleanup slurm
+rm slurm/spyrmsd/*
+rm slurm/gtyper/*
