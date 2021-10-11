08b2a568cad2de230f8c1b738a04173a95b3e43c
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-10-11 15:23:22 +0100

    make cleanup script safe by commenting everything

--- crossdocking/cd-downsampled/cleanup.sh
+++ crossdocking/cd-downsampled/cleanup.sh
@@ -8,26 +8,26 @@ for pocket in $(ls carlos_cd)
 #for pocket in AOFB
 do
     # Cleanup 03_crystal.slurm and 04_makeflex.slurm
-    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
-    rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
+    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_p*.*
+    #rm carlos_cd/${pocket}/PDB_Structures/*_default_ensemble_none_flexdist3.5*_crys*.*
     
     # Cleanup 05_spyrmsd.slurm
-    rm carlos_cd/${pocket}/PDB_Structures/*.rmsds
-    find carlos_cd/${pocket}/PDB_Structures/ -type d -name '*.d' -exec rm -r {} +
+    #rm carlos_cd/${pocket}/PDB_Structures/*.rmsds
+    #find carlos_cd/${pocket}/PDB_Structures/ -type d -name '*.d' -exec rm -r {} +
 
     # Cleanup 09_gningtyper.sh
     #  .gninatypes file are already cleaned up by commands above (without extension)
 done
-rm slurm/crystal/*
-rm slurm/makeflex/*
+#rm slurm/crystal/*
+#rm slurm/makeflex/*
 
 # Cleanup 06_collect.sh
-rm carlos_cd/rmsds.csv
+#rm carlos_cd/rmsds.csv
 
 # Cleanup 10_annotation.py
-rm files/test_*
-rm files/train_*
+#rm files/test_*
+#rm files/train_*
 
 # Cleanup slurm
-rm slurm/spyrmsd/*
-rm slurm/gtyper/*
+#rm slurm/spyrmsd/*
+#rm slurm/gtyper/*
