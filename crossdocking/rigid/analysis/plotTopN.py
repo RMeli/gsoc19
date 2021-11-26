1f640888f225b5a0f7d60f232fdbfa603e193f13
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-11-26 16:25:40 +0000

    plot for docking with flexible residues

---
+++ crossdocking/rigid/analysis/plotTopN.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import pandas as pd
+
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("allTopN.csv")
+
+df_melted = df.melt(value_vars=["best", "smina", "gnina"], id_vars=["N", "annotation","prefix","model","crystal"])
+
+print(df_melted)
+
+# Rename GNINA column with the actual model name and rename columns for plotting
+idxs = df_melted["variable"] == "gnina"
+df_melted.loc[idxs, "variable"] = df_melted.loc[idxs, "model"]
+df_melted.drop(columns="model", inplace=True)
+df_melted.rename(columns={"variable": "method", "value": "TopN (%)"}, inplace=True)
+
+print(df_melted)
+
+sns.relplot(data=df_melted, x="N", y="TopN (%)", hue="method", style="crystal", col="prefix", kind="line")
+plt.savefig("TopN.png")
