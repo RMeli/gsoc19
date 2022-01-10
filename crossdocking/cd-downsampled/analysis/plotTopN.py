90e3517703188714916fc1bd135b5e30b6bda42f
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-01-10 17:14:19 +0000

    put all topN graphs on same scale and  tight layout

--- crossdocking/cd-downsampled/analysis/plotTopN.py
+++ crossdocking/cd-downsampled/analysis/plotTopN.py
@@ -17,5 +17,7 @@ df_melted.rename(columns={"variable": "method", "value": "TopN (%)"}, inplace=Tr
 
 print(df_melted)
 
-sns.relplot(data=df_melted, x="N", y="TopN (%)", hue="method", style="crystal", col="prefix", row="annotation", kind="line", markers=True)
+g = sns.relplot(data=df_melted, x="N", y="TopN (%)", hue="method", style="crystal", col="prefix", row="annotation", kind="line", markers=True)
+g.tight_layout() # Excludes legend; plt.tight_layout() includes legend
+plt.ylim([0,100])
 plt.savefig("TopN.png")
