ebec246d44be8705e88372fcf4f0f943d081a357
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-01-10 16:01:46 +0000

    allow topN rigid to extract both rigid on flex and rigid on rigid

--- crossdocking/rigid/analysis/TopNrigid.py
+++ crossdocking/rigid/analysis/TopNrigid.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
+# Avoids duplication of code implemented in cd-downsampled/analysis/TopN.py
 import sys
 sys.path.insert(1, "../../cd-downsampled/analysis")
 
 from TopN import process
+
+# "FLEX": trained on rigid and tested on flexible
+# "": trained on rigid and tested on rigid
+pp = ""
 
 if __name__ == "__main__":
     models = ["default2017", "default2018", "dense"]
@@ -16,14 +21,14 @@ if __name__ == "__main__":
         for prefix in prefixes:
             for annotation in annotations:
                 # Not using the FLEX prefix would test the rigid docking CNN on the 
-                # rifgid docking dataset. However the TopN script has an assertion on
+                # rigid docking dataset. However the TopN script has an assertion on
                 # the number of pockets that would fail because one pocket is discarded
                 # from flexible docking for lack of actives
-                df_score, df_top = process(model, annotation, prefix, prediction_prefix="FLEX")
+                df_score, df_top = process(model, annotation, prefix, prediction_prefix=pp)
 
                 df_score_list.append(df_score)
                 df_top_list.append(df_top)
 
-    pd.concat(df_score_list).to_csv("allscores.csv", float_format="%.5f", index=False)
+    pd.concat(df_score_list).to_csv(f"{pp}allscores.csv", float_format="%.5f", index=False)
     df_top  = pd.concat(df_top_list)
-    df_top.to_csv("allTopN.csv", float_format="%.5f", index=False)
+    df_top.to_csv(f"{pp}allTopN.csv", float_format="%.5f", index=False)
