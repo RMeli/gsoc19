e71a0f867895f4692a79ecc087cf718e337fe112
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-01-31 15:07:28 +0000

    collect rmsds

--- crossdocking/cd-downsampled/07_collect.sh
+++ crossdocking/cd-downsampled/07_collect.sh
@@ -4,19 +4,20 @@
 # Collect all RMSDs and scores into a singl CSV file
 # --------------------------------------------------
 
-allscores="carlos_cd/rmsds.csv"
+allscores="carlos_cd/rmsds2.csv"
 
 # CSV Header
-csv_header="pocket,protein,ligand,rank,rmsd,obrmsd,flexrmsd,flexobrmsd,fmaxrmsd,score"
+#csv_header="pocket,protein,ligand,rank,rmsd,obrmsd,flexrmsd,flexobrmsd,fmaxrmsd,score"
+csv_header="pocket,protein,ligand,rank,rmsd,obrmsd,flexrmsd,fmaxrmsd,score"
 
 # Create a file containing all the scores
 rm -f ${allscores}
 rm -f allscores.tmp # Remove tmp file
 touch allscores.tmp # Create tmp file
-for pocket in $(ls carlos_cd)
+for pocket in $(ls -d carlos_cd/*/)
 do
     # Concatenate all score files
-    cat allscores.tmp carlos_cd/${pocket}/PDB_Structures/*.rmsds > tmp
+    cat allscores.tmp ${pocket}/PDB_Structures/*.rmsds2 > tmp
     mv tmp allscores.tmp
 done
 grep -v ${csv_header} allscores.tmp > ${allscores} # Remove all headers
