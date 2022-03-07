53c89c64f81b5baa9bff7845c60391b385ee0862
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-03-07 14:19:03 +0000

    analysis script to compute TopN metrics

---
+++ crossdocking/cd-downsampled/_14_analysis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+wdir=$PWD
+
+for model in \
+    "default2017-noaffinity-nostratified" "default2018-noaffinity-nostratified" "dense-noaffinity-nostratified" \
+    "default2017-noaffinity" "default2018-noaffinity" "dense-noaffinity"
+do
+    traindir="${wdir}/training/${model}"
+
+    cd ${traindir}
+
+    mkdir -p analysis
+
+    # scripts.plot.roc is available at https://github.com/RMeli/scripts/
+    python -m scripts.plot.roc \
+        *.?.auc.finaltrain *.?.auc.finaltest \
+        -g 0 0 0 1 1 1 \
+        -l "Train 0" "Train 1" "Train 2" "Test 0" "Test 1" "Test 2" \
+        -o analysis/auc.pdf
+
+
+    # scripts.plot.pr is available at https://github.com/RMeli/scripts/
+    python -m scripts.plot.pr \
+        *.?.auc.finaltrain *.?.auc.finaltest \
+        -g 0 0 0 1 1 1 \
+        -l "Train 0" "Train 1" "Train 2" "Test 0" "Test 1" "Test 2" \
+        -o analysis/pr.pdf
+
+    #for fold in 0 1 2
+    #do 
+    #    echo "cnn_score,smina_score,lig_rmsd,fmax_rmsd,system,smina_rank" > fulltest${fold}.csv
+    #    head -n -1 fulltest${fold}.out  | awk '{print $1","$9","$7","$8","$4}' | 
+    #        sed "s#/...._protein-#,#g" | sed 's#\.gninatypes##g' >> fulltest${fold}.csv
+    #done
+
+    #python ../dist.py fulltest[0-2].csv -o analysis/dist_b.pdf -g boxen
+    #python ../dist.py fulltest[0-2].csv -o analysis/dist_d.pdf -g distplot
+done
