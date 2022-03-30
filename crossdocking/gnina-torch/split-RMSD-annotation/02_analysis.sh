c1cc1961b2a6377368691431ef3bfb2d1c288fdd
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-03-30 14:01:36 +0100

    plotting script with strtification and different annotations

--- crossdocking/gnina-torch/split-RMSD-annotation/02_analysis.sh
+++ crossdocking/gnina-torch/split-RMSD-annotation/02_analysis.sh
@@ -2,10 +2,11 @@
 
 mkdir -p plots
 
-for model in "default2017-nc" "default2018-nc" #"dense-nc"
+for model in "default2017-nc" "default2018-nc" "dense-nc"
 do
-    for prefix in "flex1" #"flex2" "max2"
+    for prefix in "flex05" "flex1" #"flex2" "max2"
     do
         python plot.py ${model} ${prefix}
+        python plot.py ${model} ${prefix} --suffix stratified
     done
 done
