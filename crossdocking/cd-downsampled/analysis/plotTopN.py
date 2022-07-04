032becfaa2c4e1f71334b0a0ec982ef8d8916ffb
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-07-04 17:59:09 +0100

    better plots

--- crossdocking/cd-downsampled/analysis/plotTopN.py
+++ crossdocking/cd-downsampled/analysis/plotTopN.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("analysis/allTopN.csv")
+df = pd.read_csv("allTopN.csv")
 
 df_melted = df.melt(
     value_vars=["best", "smina", "gnina"],
@@ -20,18 +20,28 @@ df_melted.rename(columns={"variable": "method", "value": "TopN (%)"}, inplace=Tr
 
 print(df_melted)
 
-g = sns.relplot(
-    data=df_melted,
-    x="N",
-    y="TopN (%)",
-    hue="method",
-    style="crystal",
-    col="prefix",
-    row="annotation",
-    kind="line",
-    markers=True,
-)
-g.tight_layout()  # Excludes legend; plt.tight_layout() includes legend
-plt.ylim([0, 100])
-plt.savefig("analysis/TopN.png")
-plt.savefig("analysis/TopN.pdf")
+# Rename prefix
+df_melted["prefix"] = df_melted["prefix"].str.replace("cluster", "clustering")
+df_melted["prefix"] = df_melted["prefix"].str.replace("nc", "no clustering")
+
+
+def plot(df, name):
+    g = sns.relplot(
+        data=df,
+        x="N",
+        y="TopN (%)",
+        hue="method",
+        style="crystal",
+        col="prefix",
+        row="annotation",
+        kind="line",
+        markers=True,
+    )
+    g.tight_layout()  # Excludes legend; plt.tight_layout() includes legend
+    plt.ylim([0, 100])
+    plt.savefig(f"plots/TopN-{name}.png")
+    plt.savefig(f"plots/TopN-{name}.pdf")
+
+
+plot(df_melted[df_melted.annotation != "ligand"], "flexannotation")
+plot(df_melted[df_melted.annotation == "ligand"], "ligannotation")
